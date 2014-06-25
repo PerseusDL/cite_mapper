1b3c3699f82e5ca3ed518ec0575e8a20867eba4e
LFDM <1986gh@gmail.com>
2014-06-25 09:09:34 +0200

    Add a unit test

--- spec/lib/cts_mapper_spec.rb
+++ spec/lib/cts_mapper_spec.rb
@@ -4,4 +4,18 @@ describe CtsMapper do
   it 'has a version number' do
     expect(CtsMapper::VERSION).not_to be nil
   end
+
+  before(:all) do
+    @mapper = CtsMapper.new
+  end
+
+  describe "#find_by_cite" do
+    it "returns an object with info about the standard citation" do
+     cite = 'urn:cts:latinLit:phi0448.phi001.perseus-grc1:6'
+     res  = @mapper.find_by_cite(cite)
+     res.author.should == 'Caes.'
+     res.work.should == 'Gall.'
+     res.section.should == '6'
+    end
+  end
 end
